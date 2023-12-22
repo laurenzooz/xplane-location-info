b51af25d9e153241d8dae3456323820a1ec576b4
laurenzooz <lauri.pahkala@hotmail.com>
2023-12-22 15:27:27 +0200

    Prints the coordinates to xplane log every 10 seconds
    Added and registered flight loop, created handles for the location datarefs

--- src/location-info.cpp
+++ src/location-info.cpp
@@ -1,7 +1,11 @@
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
 #include "XPLMUtilities.h"
+#include "XPLMDataAccess.h"
+
+#include <iostream>
 #include <string.h>
+
 #if IBM
 	#include <windows.h>
 #endif
@@ -17,6 +21,29 @@
 	#error This is made to be compiled against the XPLM400 SDK
 #endif
 
+
+// coordinate dataref handles
+XPLMDataRef lat_handle = XPLMFindDataRef("sim/flightmodel/position/latitude");
+XPLMDataRef lon_handle = XPLMFindDataRef("sim/flightmodel/position/longitude");
+
+static float floop_cb(float time_since_last_call, float time_since_last_floop, int floop_counter, void* inRefcon) // vehicles
+{	
+	
+	double lat = XPLMGetDataf(lat_handle);
+	double lon = XPLMGetDataf(lon_handle);
+
+	XPLMDebugString("Current coordinates are: ");
+	XPLMDebugString(std::to_string(lat).c_str());
+	XPLMDebugString(", ");
+	XPLMDebugString(std::to_string(lon).c_str());
+	XPLMDebugString("\n");
+
+	return 10;
+}
+
+
+
+
 PLUGIN_API int XPluginStart(
 							char *		outName,
 							char *		outSig,
@@ -27,6 +54,8 @@ PLUGIN_API int XPluginStart(
 	strcpy(outDesc, "A plug-in that shows info of the location you are flying in");
 
 	XPLMDebugString("Starting Location Info plugin\n");
+
+	XPLMRegisterFlightLoopCallback(floop_cb, 10, NULL); // flight loop every 10 seconds
 	
 }
 
@@ -35,5 +64,5 @@ PLUGIN_API void	XPluginStop(void)
 }
 
 PLUGIN_API void XPluginDisable(void) { }
-PLUGIN_API int  XPluginEnable(void)  { return 1; }
+PLUGIN_API int  XPluginEnable(void)  { return 10; }
 PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
